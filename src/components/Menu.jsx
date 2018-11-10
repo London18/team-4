613b069be763f7043efb4a6d84f9976c7d64b0c0
neteduandrei <andrei.netedu2009@gmail.com>
2018-11-10 12:59:10 +0200

    Implemented menu/profile

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,29 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/es/Button/Button";
+import SettingsIcon  from '@material-ui/icons/Settings';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import NoStyleLink from "components/NoStyleLink";
+import styled from "styled-components";
+import orange from '@material-ui/core/colors/orange';
+import Profile from "./Profile";
 
 
-const styles = theme => ({
-    button: {
+const styles = {
+    settings: {
+        width: '200px',
 
-    }
-});
+        background: orange[600]
+    },
+    start: {
+
+        width: '150px',
+        height: '150px',
+        backgroundColor: orange[600]
+    },
+    settingsButton: {},
+    startButton: {}
+};
+
+const Container = styled.div`
+ text-align: center;
+ margin: 70px auto;
+`;
 
 class Menu extends React.Component {
     render() {
         return (
             <div>
-                <div className={classes.button}>
-                    <Button>
-                        Settings
-                    </Button>
+                <Profile />
+                <div>
+                    <Container>
+                        <Button style={styles.start} variant="contained" >
+                            <NoStyleLink to="/survey"></NoStyleLink>
+                            <AssignmentIcon/>
+                        </Button>
+                    </Container>
                 </div>
 
-                <div className={classes.button}>
-                    <Button>
-                        <NoStyleLink to="/survey">Survey</NoStyleLink>
-                    </Button>
+                <div>
+
+                    <Container>
+                        <Button style={styles.start} variant={"contained"} >
+                            <NoStyleLink to="/settings"></NoStyleLink>
+                            <SettingsIcon/>
+                        </Button>
+                    </Container>
                 </div>
             </div>
         );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+};
+
+class Profile extends React.Component {
+    state = {
+        completed: 70,
+        level: 11
+    };
+
+    progress = () => {
+        const { completed } = this.state;
+        this.setState({completed: this.progress});
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <br/>
+            <Grid container justify={"center"} alignItems={"center"} direction="row" spacing={24}>
+                <Grid container direction="column" justify={"center"}>
+                    <Grid item xs={12}>
+                        <h2 style={{textAlign: "center"}}>Level {this.state.level}</h2>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    <LinearProgress variant="determinate" color="secondary"  value={this.state.completed} style={{height: 30}}/>
+                </Grid>
+            </Grid>
+            </div>
+        );
+    }
+}
+
+Profile.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Profile);
