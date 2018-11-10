d3a8779f77cd2bc39940644d6d6784eda84fa708
Cosmin Pascaru <cosmin.pascaru2@gmail.com>
2018-11-10 09:57:39 +0000

    Time per question progress chart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProgressSnackBar from "./ProgressSnackBar";
 import Login from "./Login";
+import { TimePerQuestionProgressChart } from "./TimePerQuestionProgressChart";
 
 export default class Homepage extends React.Component {
     render() {
         return (
             <div>
-                <Login />
+                {/*<Login />*/}
+                <TimePerQuestionProgressChart />
             </div>
         );
     }
